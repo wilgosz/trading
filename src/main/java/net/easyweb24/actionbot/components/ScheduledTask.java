9cc9b7012c3df5d5cc41f484cdfdd2388b30a80f
zbigniewwilgosz <zbigniewwilgosz@pc2.home>
2020-09-02 14:10:57 +0200

    Add Scheduled Task

--- src/main/java/net/easyweb24/actionbot/components/ScheduledTask.java
+++ src/main/java/net/easyweb24/actionbot/components/ScheduledTask.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
  *
  * @author zbigniewwilgosz
  */
-//@Component
+@Component
 public class ScheduledTask {
    
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
